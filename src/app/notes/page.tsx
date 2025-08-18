d32e8af9d7b422427bc98c79819b7ed41d170fe0
Lio3307 <aureliofs01@gmail.com>
2025-08-18 20:01:16 +0800

    setting up for notes page

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,7 +1,37 @@
-export default function NotesPage(){
-    return (
-        <>
-        
-        </>
-    )
+"use client";
+
+import { useState } from "react";
+
+export default function NotesPage() {
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  return (
+    <main id="notes">
+      <section>
+        <div>
+          <button>Save</button>
+          {isEdit ? (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setIsEdit((prev) => !prev);
+              }}
+            >
+              Edit
+            </button>
+          ) : (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setIsEdit((prev) => !prev);
+              }}
+            >
+              View
+            </button>
+          )}
+        </div>
+      </section>
+    </main>
+  );
 }
