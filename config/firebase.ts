b0acfd87c519b6af1822640af320d947255e315f
Lio3307 <aureliofs01@gmail.com>
2025-08-19 09:22:18 +0800

    setting for auth and db for firebase

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,4 +13,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+export {auth, db}
